0284d99145361ff844b0317de121c1a004cb6db1
Nanoruri <kaby1217@gmail.com>
2023-11-08 21:24:17 +0900

    Signup page properties JPA, MariaDB/테스트 코드, pom.xml 다시하기

---
+++ src/test/java/springApplicationTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class springApplicationTest {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Test
+    public void testMariaDbConnection() {
+        // 테스트할 연결을 테스트하는 코드 작성
+        int result = jdbcTemplate.queryForObject("SELECT 1", Integer.class);
+        // 원하는 테스트를 작성하고 결과를 검증
+        assertEquals(1, result);
+    }
+    //테스트 코드 다시 짜기..
+}
+
+
