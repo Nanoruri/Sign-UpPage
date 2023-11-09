d6185f732ad457b287f869960723621116335b5a
Nanoruri <kaby1217@gmail.com>
2023-11-09 18:43:32 +0900

    Signup page properties JPA, MariaDB/테스트 코드 성공, mockito 디펜던시 추가

---
+++ src/main/java/me/SpringStudy/RepositoryDto/MeberDto.java
@@ -0,0 +1,5 @@
+package me.SpringStudy.RepositoryDto;
+
+public abstract class MeberDto {
+    //TODO : 여기부터
+}
--- src/test/java/SpringApplicationTest.java
+++ src/test/java/SpringApplicationTest.java
@@ -1,13 +1,14 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import me.SpringStudy.MySpringBootApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+@SpringBootTest(classes = MySpringBootApplication.class)
 
-@SpringBootTest
-public class springApplicationTest {
+public class SpringApplicationTest {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
@@ -15,11 +16,9 @@ public class springApplicationTest {
     @Test
     public void testMariaDbConnection() {
         // 테스트할 연결을 테스트하는 코드 작성
-        int result = jdbcTemplate.queryForObject("SELECT 1", Integer.class);
+        Integer result = jdbcTemplate.queryForObject("SELECT 1", Integer.class);
         // 원하는 테스트를 작성하고 결과를 검증
         assertEquals(1, result);
+        //테스트 성공
     }
-    //테스트 코드 오류 잡기..
 }
-
-
