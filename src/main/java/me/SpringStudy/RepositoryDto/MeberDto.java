d18a50db6dc969d5d395742a0538935859805794
Nanoruri <kaby1217@gmail.com>
2023-11-11 19:00:54 +0900

    Signup page properties JPA, MariaDB/signupPage정보의 템플릿,DTO

--- src/main/java/me/SpringStudy/RepositoryDto/MeberDto.java
+++ src/main/java/me/SpringStudy/RepositoryDto/MeberDto.java
@@ -2,11 +2,16 @@ package me.SpringStudy.RepositoryDto;
 
 public abstract class MeberDto {
     private int userNo;
+
+
     private String userId;
-    private String userPhoneNum;
-    private String userPw;
     private String userName;
+    private String userPassword;
+    private String userPhoneNum;
     private String userBirth;
+    private String userEmail;
+
+
     private String appendDate;
     private String updateDate;
 
