f8499a3f71cf9b996d612fa6626d9b6eb5fd7a4d
Nanoruri <kaby1217@gmail.com>
2023-12-02 12:20:49 +0900

    Signup page properties JPA, MariaDB/로그인 구현 아이디 찾기 중복 방지 고민

--- src/main/java/me/SpringStudy/RepositoryDao/MemberDao.java
+++ src/main/java/me/SpringStudy/RepositoryDao/MemberDao.java
@@ -11,9 +11,10 @@ import java.util.Optional;
 public interface MemberDao extends JpaRepository<Member,Long> {
 
 
-    // 중복된 아이디가 있는지 확인하는 메서드
-    boolean existsByUserId(String userId);
-
     // 특정 조건에 따른 회원 검색 메서드
     Optional<Member> findByUserId(String userId);
+
+    //중복확인 검사 메서드/JPA는 메서드 이름을 기준으로  분석하여 쿼리를 자동으로 생성함. 그래서 엔티티 테이블과 매핑할 수 있는
+    //엔티티 클래스가 필요한거라고 추즉 됨
+    boolean existByUserId(String userId);
 }
