8a130c12ef1965e03135e14f3ae2c6744c2037a3
Nanoruri <kaby1217@gmail.com>
2023-12-05 16:44:34 +0900

    Signup page properties JPA, MariaDB/ CSRF토큰 작동안함

--- src/main/java/me/SpringStudy/Config/SecurityConfig.java
+++ src/main/java/me/SpringStudy/Config/SecurityConfig.java
@@ -43,8 +43,8 @@ public class SecurityConfig  {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {//보안상의 문제로 configure에서 filterChain으로 바뀜.
-        http.csrf().disable()
-                //csrf 비활성화... 북마크 참고 할 것. 혹은 CSRF 토큰 구현..
+        http.csrf().disable()//csrf 비활성화
+                // 요즘은 CSRF만 방어하기 보단 JWT 쓴다고 함. 혹은 같이 쓰거나...
 
 
 
@@ -61,7 +61,7 @@ public class SecurityConfig  {
 //                        .loginProcessingUrl("/loginCheck") //이거 cotroller에서 구현함.로그인 Form 처리 Url, 여기를 통해 post요청이 들어감.
                         .usernameParameter("userId") // 아이디 파라미터명 설정
                         .passwordParameter("userPassword") // 패스워드 파라미터명 설정
-                        .successForwardUrl("/main") // 로그인 성공 후의 리다이렉션 URL 설정, 여기선/signup페이지로 리다이렉트
+                        .defaultSuccessUrl("/main") // 로그인 성공 후의 리다이렉션 URL 설정, 여기선/main페이지로 리다이렉트
                         .failureForwardUrl("/signupError")
                         .permitAll() //로그인 페이지 접속하는것에 대해 권한 X
 
@@ -74,13 +74,13 @@ public class SecurityConfig  {
         return http.build();
     }
 
-    @Bean
-    public CsrfTokenRepository csrfTokenRepository() {
-        HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
-        // 토큰이 저장될 파라미터명을 설정 (클라이언트에서 전송시 사용됨)
-        repository.setSessionAttributeName("_csrf");
-        return repository;//TODO : 동작 안함
-    }
+//    @Bean
+//    public CsrfTokenRepository csrfTokenRepository() {
+//        HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+//        // 토큰이 저장될 파라미터명을 설정 (클라이언트에서 전송시 사용됨)
+//        repository.setSessionAttributeName("_csrf");
+//        return repository;//TODO : 동작 안함
+//    }
 }
 
 
