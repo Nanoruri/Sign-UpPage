2e48c796b44ed21a15c22657cccfd0cc6fedf01b
Nanoruri <kaby1217@gmail.com>
2023-12-10 16:23:06 +0900

    Signup page properties JPA, MariaDB/ 테스트 코드 재작성 완료

---
+++ src/test/java/SpringApplicationTest.java
@@ -0,0 +1,32 @@
+import me.JH.SpringStudy.Entitiy.User;
+import me.JH.SpringStudy.MySpringBootApplication;
+import me.JH.SpringStudy.RepositoryDao.MemberDao;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import java.util.Date;
+
+
+@SpringBootTest(classes = MySpringBootApplication.class)
+public class SpringApplicationTest {
+
+	@Autowired
+	private MemberDao memberDao;
+
+	@Test
+	public void testMariaDbConnection() {
+		// 테스트할 연결을 테스트하는 코드 작성
+		User user = new User();
+		user.setUserId("kaby1217");
+		user.setName("Lim");
+		user.setPassword("1217159");
+		user.setBirth(new Date(19961127));
+		user.setPhoneNum("010-9525-6863");
+		user.setEmail("kaby1217@gmail.com");
+		user.setCreatedDate(new Date());
+		user.setUpdateDate(new Date());
+
+		memberDao.save(user);
+	}
+}
+
