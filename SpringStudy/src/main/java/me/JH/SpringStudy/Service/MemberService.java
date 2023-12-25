c41519166008a605b0519b2d8a6dc315012919d8
Nanoruri <kaby1217@gmail.com>
2023-12-25 19:05:32 +0900

    Signup page properties JPA, MariaDB/ 비밀번호 찾기 로직 점검하기

--- SpringStudy/src/main/java/me/JH/SpringStudy/Service/MemberService.java
+++ SpringStudy/src/main/java/me/JH/SpringStudy/Service/MemberService.java
@@ -2,8 +2,6 @@ package me.JH.SpringStudy.Service;
 
 import me.JH.SpringStudy.Entitiy.User;
 import me.JH.SpringStudy.RepositoryDao.MemberDao;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
