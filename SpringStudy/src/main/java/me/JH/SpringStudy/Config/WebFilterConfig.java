a33655c8c4648d93250b8cac7cd8761165a6156e
Nanoruri <kaby1217@gmail.com>
2024-01-06 17:43:05 +0900

    Revert "Revert "feat : LoggingFilter 구현, WebFilterConfig 추가""
    This reverts commit 2513eeeb0bda48a9def4ca2be64e8e0f53ee6946.

---
+++ SpringStudy/src/main/java/me/JH/SpringStudy/Config/WebFilterConfig.java
@@ -0,0 +1,21 @@
+package me.JH.SpringStudy.Config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebFilterConfig {
+
+	@Bean
+	public FilterRegistrationBean<LoggingFilter> loggingFilter() {
+
+		FilterRegistrationBean<LoggingFilter> registrationBean = new FilterRegistrationBean<>();
+		registrationBean.setFilter(new LoggingFilter());//등록할 필터 클래스 지정
+//		registrationBean.setOrder(1);//필터 순서 설정
+		registrationBean.addUrlPatterns("/*"); // 필터가 적용될 URL 설정. 필터를 모든 URL 패턴에 적용
+
+		return registrationBean;
+	}
+
+}
