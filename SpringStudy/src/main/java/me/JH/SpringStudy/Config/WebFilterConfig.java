e3aa462df380e433449948b640d60f35f83fedc2
Nanoruri <kaby1217@gmail.com>
2024-01-08 18:00:23 +0900

    chore : 필터 컨피그 코드 수정, 불필요한 MyFilter 클래스 삭제
    MyFilter ->LoggingFilter로 대체되었음

--- SpringStudy/src/main/java/me/JH/SpringStudy/Config/WebFilterConfig.java
+++ SpringStudy/src/main/java/me/JH/SpringStudy/Config/WebFilterConfig.java
@@ -1,5 +1,6 @@
 package me.JH.SpringStudy.Config;
 
+import me.JH.SpringStudy.Filter.LoggingFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
