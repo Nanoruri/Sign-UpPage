00a996ef382d0bda7b6cb55c8d2de68aa3d5a5f9
Nanoruri <kaby1217@gmail.com>
2024-01-08 17:54:35 +0900

    feat : Exception코드 추가

--- SpringStudy/src/main/java/me/JH/SpringStudy/Filter/LoggingFilter.java
+++ SpringStudy/src/main/java/me/JH/SpringStudy/Filter/LoggingFilter.java
@@ -1,12 +1,17 @@
-package me.JH.SpringStudy.Config;
+package me.JH.SpringStudy.Filter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.UUID;
+
+/**
+ * 요청과 응답에 대해 로그를 찍는 ServletFilter
+ */
 
 public class LoggingFilter implements Filter {
 	private static final Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
@@ -23,15 +28,26 @@ public class LoggingFilter implements Filter {
 		HttpServletRequest httpRequest = (HttpServletRequest) request;
 		String requestURI = httpRequest.getRequestURI();
 		String uuid = UUID.randomUUID().toString();
-		// 요청 전 로깅
-		logger.info("요청 [{}][{}]", uuid, requestURI);
 
-		// 다음 필터 또는 서블릿으로 요청 전달
-		chain.doFilter(request, response);
+		try {
+			// 요청 전 로깅
+			logger.info("요청 [{}][{}]", uuid, requestURI);
 
-		// 응답 후 로깅
-		logger.info("응답 [{}][{}]", uuid, requestURI);
+			// 다음 필터 또는 서블릿으로 요청 전달
+			chain.doFilter(request, response);
 
+			// 응답 후 로깅
+			logger.info("응답 [{}][{}]", uuid, requestURI);
+
+		}catch (Exception ex) {
+			// 예외 발생 시 로깅
+			logger.error("에러 발생 [{}][{}]: {}", uuid, requestURI, ex.getMessage());
+
+			// 예외에 대한 추가적인 응답 처리 가능
+			HttpServletResponse httpResponse = (HttpServletResponse) response;
+			httpResponse.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			httpResponse.getWriter().write("서버 에러");
+		}
 	}
 
 	@Override
