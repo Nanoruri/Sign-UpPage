bc0cde6d9b85b19a1c6cc4852a707b76b3a70c47
Nanoruri <kaby1217@gmail.com>
2024-02-01 16:47:56 +0900

    feat : 회원가입 페이지 비밀번호 확인 로직 구현
    signupPage.html 수정 : '비밀번호 확인' 입력 필드 추가 및 비밀번호 일치메세지 추가
    passwordValidate.js 추가 : 비밀번호 일치 로직 구현
    signup.css 수정 : signupPage의 입력필드 추가에 따른 css 조정

---
+++ SpringStudy/src/main/resources/static/js/signup/passwordValidate.js
@@ -0,0 +1,18 @@
+function passwordValidate(){ // todo : passwordValidation.js와 비슷한 기능이니 고민해서 둘중 하나로 변경하기
+    var password = document.getElementById('inputPassword').value;
+    var confirmPassword = document.getElementById('confirmPassword').value;
+    var MatchingMessage = document.getElementById('matchingMessage');
+    var signupButton = document.getElementById('signupButton');
+
+    if(password !== confirmPassword){
+        MatchingMessage.innerText = '비밀번호가 일치하지 않습니다.';
+        MatchingMessage.style.color = 'red'
+        signupButton.disabled = true;
+    } else{
+        MatchingMessage.innerText = '비밀번호가 일치합니다.'
+        MatchingMessage.style.color = 'blue'
+        signupButton.disabled = false;
+    }
+
+}
+document.getElementById('confirmPassword').addEventListener('input', passwordValidate)
