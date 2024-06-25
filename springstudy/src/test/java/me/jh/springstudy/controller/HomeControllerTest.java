de45519da02e6b465750ea9acb49ce23b5a68c92
Nanoruri <kaby1217@gmail.com>
2024-06-26 00:46:35 +0900

    test : 개선된 메인페이지 로직에 따른 테스트 코드 수정
    - 메인페이지 로직 인증된 사용자 정보 유무에 따른 테스트 케이스 수정

--- springstudy/src/test/java/me/jh/springstudy/controller/HomeControllerTest.java
+++ springstudy/src/test/java/me/jh/springstudy/controller/HomeControllerTest.java
@@ -1,6 +1,7 @@
 package me.jh.springstudy.controller;
 
 import me.jh.springstudy.config.SecurityConfig;
+import me.jh.springstudy.service.userservice.CustomUserDetailsService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -8,6 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -26,8 +31,15 @@ public class HomeControllerTest {
 	@Autowired
 	private MockMvc mockMvc;
 
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+//	@MockBean
+//	private HttpSession session;
+
 	@MockBean
-	private HttpSession session;
+	private CustomUserDetailsService customUserDetailsService;
+
 
 
 	@Test
@@ -40,18 +52,31 @@ public class HomeControllerTest {
 	@Test
 	public void testIndexFormWhenLogin() throws Exception {
 
-		when(session.getAttribute("userId")).thenReturn("testUser");
 
+//		when(session.getAttribute("userId")).thenReturn("testUser");
+		//UserDetails 객체 생성
+		UserDetails userDetails = User.builder()
+				.username("test")
+				.password(passwordEncoder.encode("test"))
+				.roles("USER")
+				.build();
+
+
+		//UserDetailsService에서 loadUserByUsername 메서드를 호출하면 userDetails 객체를 반환하도록 설정
 		mockMvc.perform(get("/")
-						.sessionAttr("userId", "testUser"))
+//						.sessionAttr("userId", "testUser"))
+						.with(SecurityMockMvcRequestPostProcessors.user(userDetails)))//인증된 사용자 정보를 전달
 				.andExpect(status().isOk())
 				.andExpect(view().name("index"))
-				.andExpect(model().attribute("LoggedIn", true))
-				.andExpect(request().sessionAttribute("userId", "testUser"));
+				.andExpect(model().attribute("LoggedIn", true));
+//				.andExpect(request().sessionAttribute("userId", "testUser"));
 	}
 
 	@Test
 	public void testIndexFormWhenLogout() throws Exception {
+
+		//로그인하지 않은 사용자의 경우
+
 		mockMvc.perform(get("/"))
 				.andExpect(status().isOk())
 				.andExpect(view().name("index"))
