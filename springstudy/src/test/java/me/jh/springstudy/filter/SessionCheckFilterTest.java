09911d85ad76d7bca0580d195bf82a9c8c8866cc
Nanoruri <kaby1217@gmail.com>
2024-06-28 00:31:56 +0900

    test : SessionCheckFilterTest코드 리팩토링
    테스트에 필요한 객체를 매번 생성하는 방식에서 @Mock, @InjectionMock을 통해 코드의 가독성, 유지보수성 향상

--- springstudy/src/test/java/me/jh/springstudy/filter/SessionCheckFilterTest.java
+++ springstudy/src/test/java/me/jh/springstudy/filter/SessionCheckFilterTest.java
@@ -3,6 +3,8 @@ package me.jh.springstudy.filter;
 import me.jh.springstudy.MySpringBootApplication;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -20,55 +22,59 @@ import static org.mockito.Mockito.*;
 @SpringBootTest(classes = MySpringBootApplication.class)
 public class SessionCheckFilterTest {
 
+
+	@Mock
+	private HttpServletRequest request;
+	@Mock
+	private HttpServletResponse response;
+	@Mock
+	private FilterChain chain;
+	@Mock
+	private HttpSession session;
+
+	@InjectMocks
+	private SessionCheckFilter sessionCheckFilter;
+
 	@Test
 	public void testSessionCheck() throws ServletException, IOException {
 
 		//  HttpServletRequest, HttpServletResponse, FilterChain, Session Mock객체 생성
-		HttpServletRequest request = mock(HttpServletRequest.class);
-		HttpServletResponse response = mock(HttpServletResponse.class);
-		FilterChain chain = mock(FilterChain.class);
-		HttpSession session = mock(HttpSession.class);
 
 		//테스트 조건 설정
 		when(request.getSession()).thenReturn(session);
-		String sessionName = "Study";
-		String sessionValue = "signupProject";
-		when(session.getAttribute(sessionName)).thenReturn(sessionValue);
-
+		when(session.getAttribute("Study")).thenReturn("signupProject");
 
 		// CookieCheckFilter 인스턴스 생성
-		SessionCheckFilter filter = new SessionCheckFilter();
+
 
 		// 필터 테스트
-		filter.doFilter(request, response, chain);
+		sessionCheckFilter.doFilter(request, response, chain);
 
 		// 다음 필터 호출 확인
+		verify(session).getAttribute("Study");
 		verify(chain).doFilter(request, response);
 	}
 
 	@Test
-	public void testSessionCheckException() throws ServletException, IOException {
+	public void testSessionCheckExceptionAttributeNull() throws ServletException, IOException {
 
 		//  HttpServletRequest, HttpServletResponse, FilterChain, Session Mock객체 생성
-		HttpServletRequest request = mock(HttpServletRequest.class);
-		HttpServletResponse response = mock(HttpServletResponse.class);
-		FilterChain chain = mock(FilterChain.class);
-		HttpSession session = mock(HttpSession.class);
 
-		//테스트 조건 설정
+
+		//세션의 속성값이 없을때
 		when(request.getSession()).thenReturn(session);
 		when(session.getAttribute("Study")).thenReturn(null);
 //		when(request.getHeader("Cookie")).thenReturn(null);
 
-		// CookieCheckFilter 인스턴스 생성
-		SessionCheckFilter filter = new SessionCheckFilter();
 
 		// 필터 테스트
-		filter.doFilter(request,response,chain);
+		sessionCheckFilter.doFilter(request, response, chain);
 
 		// 필터에서 에러가 발생했을 때, 다음 필터 호출이 되지 않았는지 확인
 		verify(session, times(1)).getAttribute("Study");
-		verify(response).sendError(HttpServletResponse.SC_BAD_REQUEST,"필수 세션 속성이 누락되었습니다.");
+		verify(response).sendError(HttpServletResponse.SC_BAD_REQUEST, "필수 세션 속성이 누락되었습니다.");
+		verify(chain, never()).doFilter(request, response);
+	}
 		verify(chain, never()).doFilter(request, response);
 	}
 }
