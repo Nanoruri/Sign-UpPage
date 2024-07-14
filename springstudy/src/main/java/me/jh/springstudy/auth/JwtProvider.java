4b6522561ebf66a5d375624ea9f7f866cfb56e50
Nanoruri <kaby1217@gmail.com>
2024-07-14 10:20:22 +0900

    feat : JWT 토큰 검증 클래스 추가
    - JwtProvider 클래스 작성
      - JWT 토큰 복호화 및 인증 정보 추출
      - JWT 토큰 유효성 검사 메서드 구현
      - 예외 처리 및 로깅 추가
    
    - JwtProviderTest 클래스 작성
      - 다양한 케이스에 대한 JWT 토큰 검증 테스트 추가
      - 유효한 토큰, 유효하지 않은 토큰, 만료된 토큰, 지원되지 않는 토큰, 빈 토큰에 대한 테스트

---
+++ springstudy/src/main/java/me/jh/springstudy/auth/JwtProvider.java
@@ -0,0 +1,98 @@
+package me.jh.springstudy.auth;
+
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Component
+public class JwtProvider {
+
+    private final String SECRET_KEY;
+    private static final Logger log = LoggerFactory.getLogger(JwtProvider.class);
+
+
+    @Autowired
+    public JwtProvider(@Value("${jwt.secret}") String secretKey) {
+        this.SECRET_KEY = secretKey;
+    }
+
+
+
+    public Authentication getAuthentication(String accessToken) {
+        // Jwt 토큰 복호화
+        Claims claims = parseClaims(accessToken);
+
+        if (claims.get("role") == null) {
+            throw new RuntimeException("권한 정보가 없는 토큰입니다.");
+        }
+
+        // 클레임에서 권한 정보 가져오기
+        Collection<? extends GrantedAuthority> authorities = Arrays.stream(claims
+                        .get("role")
+                        .toString()
+                        .replace("[{authority=", "")
+                        .replace("}]", "")
+                        .split(","))//TODO: 여기에 권한정보..?
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+
+
+        UserDetails principal = new User(claims.getSubject(), "", authorities);
+        return new UsernamePasswordAuthenticationToken(principal, "", authorities);
+    }
+
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser()
+                    .verifyWith(getSigningKey())
+                    .build()
+                    .parseSignedClaims(token);
+            return true;
+        } catch (SecurityException | MalformedJwtException e) {
+            throw new JwtException("유효하지 않은 토큰입니다.", e);
+        } catch (ExpiredJwtException e) {
+            throw new JwtException("만료된 토큰입니다.", e);
+        } catch (UnsupportedJwtException e) {
+            throw new JwtException("지원되지 않는 토큰입니다.", e);
+        } catch (IllegalArgumentException e) {
+            throw new JwtException("토큰이 비어있습니다.", e);
+        }
+    }
+
+
+    private Claims parseClaims(String accessToken) {
+        try {
+            return Jwts.parser()
+		            .verifyWith(getSigningKey())
+		            .build()
+		            .parseSignedClaims(accessToken)
+                    .getPayload();
+        } catch (io.jsonwebtoken.ExpiredJwtException e) {
+            return e.getClaims();
+        }
+    }
+
+    private SecretKey getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(this.SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
+
---
+++ springstudy/src/test/java/me/jh/springstudy/auth/JwtProviderTest.java
@@ -0,0 +1,158 @@
+package me.jh.springstudy.auth;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Encoders;
+import io.jsonwebtoken.security.Keys;
+import me.jh.springstudy.dto.JWToken;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.crypto.SecretKey;
+import java.security.SecureRandom;
+import java.util.Collections;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+public class JwtProviderTest {
+
+	@Mock
+	JwtGenerator jwtGenerator;
+
+	@Mock
+	Authentication authentication;
+
+	@Mock
+	JWToken token;
+
+	@InjectMocks
+	private JwtProvider jwtProvider;
+
+	private SecretKey key;
+
+	private String validToken;
+
+	@BeforeEach
+	public void setup() {
+		MockitoAnnotations.openMocks(this);
+		byte[] keyBytes = new byte[64]; // 512 bits for HS512
+		new SecureRandom().nextBytes(keyBytes);
+		key = Keys.hmacShaKeyFor(keyBytes);
+		String encodedKey = Encoders.BASE64.encode(key.getEncoded());
+
+		jwtGenerator = new JwtGenerator(encodedKey);
+		jwtProvider = new JwtProvider(encodedKey);
+
+	}
+
+	@Test
+	public void testGetAuthentication_ValidToken() {
+
+		UserDetails userDetails = new User("testUser", "", Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER")));
+
+		when(authentication.getPrincipal()).thenReturn(userDetails);
+		token = jwtGenerator.generateToken(authentication);
+		validToken = token.getAccessToken();
+
+		Authentication result = jwtProvider.getAuthentication(validToken);
+
+		assertNotNull(result);
+		assertEquals("testUser", result.getName());
+		assertTrue(result.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_USER")));
+	}
+
+	@Test
+	public void testValidateToken_ValidToken() {
+		UserDetails userDetails = new User("testUser", "", Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER")));
+
+		when(authentication.getPrincipal()).thenReturn(userDetails);
+		token = jwtGenerator.generateToken(authentication);
+		validToken = token.getAccessToken();
+
+		Authentication result = jwtProvider.getAuthentication(validToken);
+
+		// When
+		boolean isValid = jwtProvider.validateToken(validToken);
+
+		// Then
+		assertTrue(isValid);
+	}
+
+	@Test
+	public void testValidateToken_InvalidToken() {
+		String invalidToken = "invalid.token";
+
+		try {
+			// When
+			jwtProvider.validateToken(invalidToken);
+			fail("JwtException이 발생해야 합니다.");
+		} catch (JwtException e) {
+			// Then
+			assertEquals("유효하지 않은 토큰입니다.", e.getMessage());
+		}
+	}
+
+
+	@Test
+	public void testValidateToken_ExpiredToken() {
+		// Arrange
+		String generateExpiredToken = Jwts.builder()
+				.subject("user")
+				.claim("role", "user")
+				.expiration(new Date(System.currentTimeMillis() - 1000)) // expired one second ago
+				.signWith(key)
+				.compact();
+
+		try {
+			// When
+			jwtProvider.validateToken(generateExpiredToken);
+			fail("ExpiredTokenException이 발생해야 합니다.");
+		} catch (JwtException e) {
+			// Then
+			assertEquals("만료된 토큰입니다.", e.getMessage());
+		}
+
+	}
+
+
+	@Test
+	public void testValidateToken_UnsupportedToken_ReturnsFalse() {
+
+		String unsupportedToken = Jwts.builder()
+				.content("Unsupported Token")
+				.signWith(key) // 지원하지 않는 서명 알고리즘 사용
+				.compact();
+
+		try {
+			// When
+			jwtProvider.validateToken(unsupportedToken);
+			fail("UnsupportedJwtException이 발생해야 합니다.");
+		} catch (JwtException e) {
+			// Then
+			assertEquals("지원되지 않는 토큰입니다.", e.getMessage());
+		}
+
+
+	}
+
+
+	@Test
+	public void testValidateToken_IllegalArgumentException_ReturnsFalse() {
+
+		try {
+			jwtProvider.validateToken(null);
+		} catch (JwtException e) {
+			assertEquals("토큰이 비어있습니다.", e.getMessage());
+		}
+
+	}
+}
