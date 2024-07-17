768865b63743452625e4990492273360ea9284b8
Nanoruri <kaby1217@gmail.com>
2024-07-18 00:24:57 +0900

    refactor : checkLogout JS파일 로직 수정
    - 토큰 존재 여부에 따라 로그인 버튼이 로그아웃 버튼으로 변하도록 기준 변경
    - 로그인 상태에서 로그아웃 시 sessionStorage에서 토큰 제거
    - updateLoginStatus함수를 추가하여 유지보수성 개선
    - index.html에서 로그인 여부를 검사하기 위한 사용하던 isLoggedIn 변수 제거

--- springstudy/src/main/resources/static/js/checkLogout.js
+++ springstudy/src/main/resources/static/js/checkLogout.js
@@ -1,27 +1,38 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var loginLogoutButtonDiv = document.getElementById('loginLogoutButton');
+    // 초기 로그인 상태 확인
+    const isLoggedIn = sessionStorage.getItem('aToken') !== null;
+        updateLoginStatus(isLoggedIn);
 
-    if (typeof isLoggedIn !== 'undefined' && isLoggedIn) {
-        // 로그인 상태일 때 로그아웃 버튼 추가
-        var logoutForm = document.createElement('form');
-        logoutForm.method = 'post';
-        logoutForm.action = '/study/logout';
-        var logoutButton = document.createElement('button');
-        logoutButton.type = 'submit';
-        logoutButton.className = 'btn btn-danger border-2';
-        logoutButton.textContent = '로그아웃';
-        logoutForm.appendChild(logoutButton);
-        loginLogoutButtonDiv.appendChild(logoutForm);
-    } else {
-        // 로그아웃 상태일 때 로그인 버튼 추가
-        var loginForm = document.createElement('form');
-        loginForm.method = 'get';
-        loginForm.action = '/study/login';
-        var loginButton = document.createElement('button');
-        loginButton.type = 'submit';
-        loginButton.className = 'btn btn-primary border-2';
-        loginButton.textContent = '로그인';
-        loginForm.appendChild(loginButton);
-        loginLogoutButtonDiv.appendChild(loginForm);
+    function updateLoginStatus(isLoggedIn) {
+        loginLogoutButtonDiv.innerHTML = '';
+
+
+
+        if (isLoggedIn) {
+            // 로그인 상태일 때 로그아웃 버튼 추가
+            var logoutForm = document.createElement('form');
+            logoutForm.method = 'post';
+            logoutForm.action = '/study/logout';
+            var logoutButton = document.createElement('button');
+            logoutButton.type = 'submit';
+            logoutButton.className = 'btn btn-danger border-2';
+            logoutButton.textContent = '로그아웃';
+            logoutForm.appendChild(logoutButton);
+            loginLogoutButtonDiv.appendChild(logoutForm);
+            sessionStorage.removeItem('aToken');
+        } else {
+            // 로그아웃 상태일 때 로그인 버튼 추가
+            var loginForm = document.createElement('form');
+            loginForm.method = 'get';
+            loginForm.action = '/study/login';
+            var loginButton = document.createElement('button');
+            loginButton.type = 'submit';
+            loginButton.className = 'btn btn-primary border-2';
+            loginButton.textContent = '로그인';
+            loginForm.appendChild(loginButton);
+            loginLogoutButtonDiv.appendChild(loginForm);
+        }
+
     }
 });
