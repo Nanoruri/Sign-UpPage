7b107908c85d3187ba55fe40a1058fc8bafac865
Nanoruri <kaby1217@gmail.com>
2024-07-23 21:23:01 +0900

    refactor : JwtAuthenticationFilter를 OncePerRequestFilter로 변경하여 요청당 한 번만 필터를 적용하도록 개선
    - JwtAuthenticationFilter를 GenericFilterBean에서 OncePerRequestFilter로 변경
    - setAuthentication 메서드를 추가하여  인증 정보 설정하도록 개선
    - 리프레시 토큰 검증 관련 코드 제거

--- springstudy/src/main/java/me/jh/springstudy/filter/JwtAuthenticationFilter.java
+++ springstudy/src/main/java/me/jh/springstudy/filter/JwtAuthenticationFilter.java
@@ -7,28 +7,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
-import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.net.http.HttpRequest;
 
 
 /**
- * JWT 토큰을 검증하는 필터 클래스.
- * 해당 클래스는 {@link JwtProvider}를 통해 토큰을 검증하고, 검증이 완료되면 SecurityContextHolder에 인증 정보를 저장한다.
- * 해당 클래스는 {@link SecurityConfig}에서 설정한 URL 패턴에 대해 동작한다.
- * 토큰이 없거나 유효하지 않다면 401 Unauthorized 에러를 반환한다.
+ * JWT 토큰을 검증하고 인증 정보를 설정하는 필터입니다. 이 필터는 요청이 들어올 때마다 한 번씩 실행됩니다.
+ * {@link JwtProvider}를 사용하여 전달받은 토큰의 유효성을 검증하고, 유효한 경우 {@link SecurityContextHolder}에 인증 정보를 저장합니다.
+ * 유효하지 않은 토큰인 경우, 401 Unauthorized 에러를 반환합니다.
+ * 이 필터는 {@link SecurityConfig}에 의해 특정 URL 패턴에 대해 적용됩니다.
  *
- * @see JwtProvider
- * @see SecurityContextHolder
- */ // todo : onceperrequestfilter는 어떻게 동작되는지 봅시다.
-public class JwtAuthenticationFilter extends GenericFilterBean {
+ * @see JwtProvider JWT 토큰 생성 및 검증을 담당하는 클래스
+ * @see SecurityContextHolder 인증 정보를 저장하고 관리하는 Spring Security의 클래스
+ */
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
 	private final JwtProvider jwtTokenProvider;
 
@@ -38,40 +35,30 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
 	}
 
 	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-			throws IOException, ServletException {
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-		HttpServletRequest httpRequest = (HttpServletRequest) request;
-		String token = resolveToken(httpRequest);
+		String token = resolveToken(request);
+
 		try {
 			if (token != null && jwtTokenProvider.validateToken(token)) {
-				setAuthentication(token, httpRequest);
+				// 리프레시 토큰일 경우, 인증 정보 설정 없이 계속 진행
+				Authentication authentication = jwtTokenProvider.getAuthentication(token);
+				SecurityContextHolder.getContext().setAuthentication(authentication);
 			}
-			chain.doFilter(request, response);
+			filterChain.doFilter(request, response);
 		} catch (JwtException e) {
 			SecurityContextHolder.clearContext();
-			((HttpServletResponse) response).sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
+			response.sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
 		}
+
 	}
 
 	private String resolveToken(HttpServletRequest request) {
 		String bearerToken = request.getHeader("Authorization");
-		String refreshToken = request.getHeader("refreshToken");
 		if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
 			return bearerToken.substring(7);
-		} else if (StringUtils.hasText(refreshToken)) {
-			return refreshToken;
 		}
 		return null;
 	}
 
-	private void setAuthentication(String token, HttpServletRequest request) {
-		Authentication authentication;
-		if (request.getHeader("refreshToken") != null) {
-			authentication = jwtTokenProvider.getAuthenticationFromRefreshToken(token);
-		} else {
-			authentication = jwtTokenProvider.getAuthentication(token);
-		}
-		SecurityContextHolder.getContext().setAuthentication(authentication);
-	}
 }
