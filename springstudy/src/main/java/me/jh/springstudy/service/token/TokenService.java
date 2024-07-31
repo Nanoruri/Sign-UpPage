da5e6466f03a2315a31de5b97c115aab4afc091e
Nanoruri <kaby1217@gmail.com>
2024-07-31 21:01:11 +0900

    feat: 리프레시 토큰을 관리하는 서비스 클래스 추가
    - TokenService 클래스 생성하여 리프레시 토큰 관리 기능 구현
      - `saveRefreshToken` 메서드: 사용자 ID와 리프레시 토큰을 저장하는 기능 추가
        - 사용자 ID나 토큰이 null 또는 빈 값인 경우 예외 처리
    
      - `matchRefreshToken` 메서드: 리프레시 토큰을 검증하고 해당 사용자 ID를 반환하는 기능 추가
        - DB내 토큰테이블에 저장된 토큰의 사용자아이디 정보를 가져와 사용자테이블과 매핑이 되는지 확인
        - 토큰이 DB에 존재하지 않는 경우 예외 처리
        - 토큰에 해당하는 사용자 ID가 DB에 존재하지 않는 경우 예외 처리
    
      - `deleteRefreshToken` 메서드: 리프레시 토큰을 삭제하는 기능 추가
        - 토큰이 null 또는 빈 값인 경우 예외 처리

---
+++ springstudy/src/main/java/me/jh/springstudy/service/token/TokenService.java
@@ -0,0 +1,61 @@
+package me.jh.springstudy.service.token;
+
+import io.jsonwebtoken.JwtException;
+import me.jh.springstudy.dao.UserDao;
+import me.jh.springstudy.dao.auth.RefreshTokenDao;
+import me.jh.springstudy.entitiy.auth.RefreshToken;
+import me.jh.springstudy.exception.user.UserErrorType;
+import me.jh.springstudy.exception.user.UserException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+
+@Service
+public class TokenService {
+
+	private final RefreshTokenDao refreshTokenDao;
+	private final UserDao userDao;
+
+	@Autowired
+	public TokenService(RefreshTokenDao refreshTokenDao, UserDao userDao) {
+		this.refreshTokenDao = refreshTokenDao;
+		this.userDao = userDao;
+	}
+
+
+	public void saveRefreshToken(String userId, String token) {
+
+		if (userId == null || token == null) {
+			throw new UserException(UserErrorType.MISSING_INFORMATION);
+		} else if (userId.isEmpty() || token.isEmpty() ) {
+			throw new UserException(UserErrorType.MISSING_INFORMATION);
+		}
+
+		refreshTokenDao.save(new RefreshToken(userId, token));
+	}
+
+
+	public String matchRefreshToken(String token) {//fixMe: DB가 아파할거같은 메서드. 개선하자
+		Optional<RefreshToken> getRefreshToken = refreshTokenDao.findById(token);
+		if (getRefreshToken.isEmpty()) {
+			throw new JwtException("Refresh Token이 존재하지 않습니다.");
+		}
+		RefreshToken tokenInfo = getRefreshToken.get();
+		if (!userDao.existsById(tokenInfo.getUserId())) {
+			throw new UserException(UserErrorType.USER_NOT_FOUND);
+		}
+
+		return tokenInfo.getUserId();
+	}
+
+	public void deleteRefreshToken(String refreshToken) {
+
+		if (refreshToken == null || refreshToken.isEmpty()) {
+			throw new UserException(UserErrorType.MISSING_INFORMATION);
+		}
+
+		refreshTokenDao.deleteById(refreshToken);
+	}
+}
