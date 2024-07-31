1c8f6da4143e340410c1a1bf1ef030c32411c1d3
Nanoruri <kaby1217@gmail.com>
2024-07-31 15:45:48 +0900

    feat: 리프레시 토큰 DAO 인터페이스 추가
    `RefreshTokenDao` 인터페이스 추가
    - 리프레시 토큰 엔티티의 데이터베이스 작업을 처리하기 위한 JPA 리포지토리
    - 리프레시 토큰을 데이터베이스에서 관리하여 보안성과 관리 용이성 향상

---
+++ springstudy/src/main/java/me/jh/springstudy/dao/auth/RefreshTokenDao.java
@@ -0,0 +1,10 @@
+package me.jh.springstudy.dao.auth;
+
+import me.jh.springstudy.entitiy.auth.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RefreshTokenDao extends JpaRepository<RefreshToken, String> {
+
+}
