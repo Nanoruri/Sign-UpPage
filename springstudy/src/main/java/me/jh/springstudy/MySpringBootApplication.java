2d33a299490ade9ec7cbb8761f48d5260598bb1d
Nanoruri <kaby1217@gmail.com>
2024-09-22 11:56:04 +0900

    refactor: 스프링 Main클래스 및 의존성 수정
    - spring 메인클래스에 어노테이션을 통해 board모듈까지 스캔하도록  변경
    - 의존성 수정을 통해 정상적으로 board모듈을 읽어 올 수 있도록 변경

--- springstudy/src/main/java/me/jh/springstudy/MySpringBootApplication.java
+++ springstudy/src/main/java/me/jh/springstudy/MySpringBootApplication.java
@@ -2,8 +2,12 @@ package me.jh.springstudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"me.jh.springstudy", "me.jh.board"})
+@EnableJpaRepositories(basePackages = {"me.jh.springstudy.dao", "me.jh.board.dao"})
+@EntityScan(basePackages = {"me.jh.springstudy.entitiy", "me.jh.board.entity"})
 public class MySpringBootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MySpringBootApplication.class, args);
