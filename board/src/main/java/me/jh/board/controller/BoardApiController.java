6b5b9cfd37843e46b10d33eb0d91a2a44b9c79cc
Nanoruri <kaby1217@gmail.com>
2024-09-25 01:13:32 +0900

    feat: 게시글 CRUD API 구현
    - BoardApiController 추가 및 CRUD 기능 구현
      - `POST /create`: 게시글 생성 API
      - `GET /read`: 모든 게시글 조회 API
      - `GET /search_content`: 제목으로 게시글 검색 API
      - `PUT /update`: 게시글 수정 API
      - `DELETE /delete/{boardId}`: ID로 게시글 삭제 API

---
+++ board/src/main/java/me/jh/board/controller/BoardApiController.java
@@ -0,0 +1,73 @@
+package me.jh.board.controller;
+
+import me.jh.board.entity.Board;
+import me.jh.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Controller
+@RequestMapping("/board/api")
+public class BoardApiController {
+
+	@PostConstruct
+	public void init() {
+		System.out.println("BoardApiController Bean 생성");
+	}
+
+	private final BoardService boardService;
+
+	@Autowired
+	public BoardApiController(BoardService boardService) {
+		this.boardService = boardService;
+	}
+
+
+	//CRUD에 대한 API 작성
+	//Create
+	@PostMapping("/create")
+	@ResponseBody
+	public ResponseEntity<Board> saveBoard(@RequestBody Board board) {
+		boardService.saveBoard(board);
+		return ResponseEntity.ok().build();
+	}
+
+
+	//Read
+	@GetMapping("/read")
+	@ResponseBody
+	public ResponseEntity<List<Board>> getBoard() {
+		List<Board> boards = boardService.getAllBoard();
+		return ResponseEntity.ok(boards);
+	}
+
+	@GetMapping("/search_content")
+	@ResponseBody
+	public ResponseEntity<Board> getBoardByContent(@RequestBody Board board) {
+		boardService.getBoardByTitle(board.getTitle());
+		return ResponseEntity.ok().build();
+	}
+
+	//Update
+	@PutMapping("/update")
+	@ResponseBody
+	public ResponseEntity<Board> updateBoard(@RequestBody Board board) {
+		boardService.updateBoard(board);
+		return ResponseEntity.ok().build();
+	}
+
+
+	//Delete
+	@DeleteMapping("/delete/{boardId}")
+	@ResponseBody
+	public ResponseEntity<Board> deleteBoard(@PathVariable Long boardId) {
+		boardService.deleteBoard(boardId);
+		return ResponseEntity.noContent().build();
+	}
+
+
+}
---
+++ board/src/test/java/me/jh/board/controller/BoardApiControllerTest.java
@@ -0,0 +1,129 @@
+package me.jh.board.controller;
+
+import me.jh.board.dao.BoardDao;
+import me.jh.board.entity.Board;
+import me.jh.board.service.BoardService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+
+@ContextConfiguration(classes = {BoardApiController.class})
+@WebMvcTest(controllers = BoardApiController.class)
+@AutoConfigureMockMvc(addFilters = false)// csrf 비활성화
+public class BoardApiControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private BoardDao boardDao;
+
+	@MockBean
+	private BoardService boardService;
+
+	@BeforeEach
+	void setup() {
+		mockMvc = standaloneSetup(new BoardApiController(boardService)).build();
+	}
+
+
+
+	@Test
+	public void saveBoardTest() throws Exception {
+		long id = 1L;
+		String title = "title";
+		String content = "content";
+		LocalDateTime date = LocalDateTime.now();
+
+		Board post = new Board(id, title, content, date);
+
+		when(boardService.saveBoard(post)).thenReturn(true);
+
+
+		mockMvc.perform(post("/board/api/create")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content("{\"id\":1,\"title\":\"" + title + "\",\"content\":\"" + content + "\",\"date\":\"" + date + "\"}"))
+				.andExpect(status().isOk());
+	}
+
+
+	@Test
+	public void findAllTest() throws Exception {
+
+		Board post = new Board(1L, "title", "content", LocalDateTime.now());
+
+		boardDao.save(post);
+
+		when(boardService.getAllBoard()).thenReturn(List.of(post));
+
+		mockMvc.perform(get("/board/api/read"))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	public void findByTitleTest() throws Exception {
+		long id = 1L;
+		String title = "title";
+		String content = "content";
+		LocalDateTime date = LocalDateTime.now();
+
+		Board post = new Board(id, title, content, date);
+
+		when(boardService.getBoardByTitle(title)).thenReturn(post);
+
+		mockMvc.perform(get("/board/api/search_content")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content("{\"id\":1,\"title\":\"" + title + "\"}"))//title만 으로 검색하였을 때..
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	public void updateBoardTest() throws Exception {
+		long id = 1L;
+		String title = "title";
+		String content = "content";
+		LocalDateTime date = LocalDateTime.now();
+
+		Board post = new Board(id, title, content, date);
+
+		when(boardService.updateBoard(post)).thenReturn(true);
+
+		mockMvc.perform(put("/board/api/update")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content("{\"id\":1,\"title\":\"" + title + "\",\"content\":\"" + content + "\",\"date\":\"" + date + "\"}"))
+				.andExpect(status().isOk());
+	}
+
+	//Delete
+	@Test
+	public void deleteBoardTest() throws Exception {
+		long id = 1L;
+		String title = "title";
+		String content = "content";
+		LocalDateTime date = LocalDateTime.now();
+
+		Board post = new Board(id, title, content, date);
+
+		when(boardService.deleteBoard(post.getId())).thenReturn(true);
+
+		mockMvc.perform(delete("/board/api/delete/{boardId}", post.getId()))
+				.andExpect(status().isNoContent());
+	}
+
+}
