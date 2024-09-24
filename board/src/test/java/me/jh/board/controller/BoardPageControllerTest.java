c5d323ef322b50d553adc28d53bddf148288161a
Nanoruri <kaby1217@gmail.com>
2024-09-24 18:42:14 +0900

    feat: 게시판 메인페이지 추가
    - 게시판 메인페이지 html 추가
    - 게시판 기본 구조 구성
    - 페이지 get요청 테스트 코드 추가

---
+++ board/src/test/java/me/jh/board/controller/BoardPageControllerTest.java
@@ -0,0 +1,29 @@
+package me.jh.board.controller;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ContextConfiguration(classes = {BoardPageController.class})
+@WebMvcTest(controllers = BoardPageController.class)
+@AutoConfigureMockMvc(addFilters = false)// csrf 비활성화
+public class BoardPageControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+
+	@Test
+	public void board() throws Exception {
+		mockMvc.perform(get("/board/page/boardIndex"))
+				.andExpect(status().isOk());
+	}
+
+}
