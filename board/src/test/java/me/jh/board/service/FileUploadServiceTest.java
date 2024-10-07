ac2f00fd78ebf876f412760530fe6097cbe317fc
Nanoruri <kaby1217@gmail.com>
2024-10-07 23:31:31 +0900

    test: FileUploadService 테스트 코드 개선
    - Mock을 이용하여 File 객체 테스트 추가
    - 운영체제에 따른 파일 저장 경로 테스트 추가
    - 실제 경로와 파일을 테스트 경로로 분리하여 테스트 환경 개선
    - 파일 업로드, 다운로드, 파일 읽기 및 UUID 생성 관련 테스트 수정 및 보완

--- board/src/test/java/me/jh/board/service/FileUploadServiceTest.java
+++ board/src/test/java/me/jh/board/service/FileUploadServiceTest.java
@@ -2,9 +2,11 @@ package me.jh.board.service;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -16,151 +18,173 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 public class FileUploadServiceTest {
 
 
-    @Mock
-    private MultipartFile multipartFile;
+	@Mock
+	private MultipartFile multipartFile;
 
-    @InjectMocks
-    private FileUploadService fileUploadService;
+	@Mock
+	private File file;
 
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
-
-    @Test
-    void testUploadImageSuccessfully() throws IOException {
-        String originalFilename = "test.jpg";
+	@Spy
+	@InjectMocks
+	private FileUploadService fileUploadService;
 
 
-        when(multipartFile.getOriginalFilename()).thenReturn(originalFilename);
-        doNothing().when(multipartFile).transferTo(any(File.class));
 
-        String result = fileUploadService.uploadImage(new MultipartFile[]{multipartFile});
+	@BeforeEach
+	void setUp() {
 
-        assertNotNull(result);  // 결과가 null이 아님을 확인
-        assertTrue(result.endsWith("_" + originalFilename)); // 결과 파일 이름이 기대대로 형성되었는지 확인
-        assertEquals(36 + 1 + originalFilename.length(), result.length());
-    }
+	}
 
-    @Test
-    void testUploadImageFails() throws IOException {
-        when(multipartFile.getOriginalFilename()).thenReturn("test.jpg");
-        doThrow(new IOException("File upload error")).when(multipartFile).transferTo(any(File.class));
-
-        RuntimeException exception = assertThrows(RuntimeException.class, () -> {
-            fileUploadService.uploadImage(new MultipartFile[]{multipartFile});
-        });
-
-        assertEquals("파일 업로드에 실패했습니다.", exception.getMessage());
-    }
-
-    @Test// 이거 고치기
-    void downloadImageSuccessfully() {
-        String fileName = "test.jpg";
-        File file = new File(fileUploadService.getSavePath(), fileName);
+	@Test
+	void testUploadImageSuccessfully() throws IOException {
+		String originalFilename = "test.jpg";
+		String savePath = System.getProperty("user.dir") + "/src/test/files/image";
 
 
-        File result = fileUploadService.downloadImage(fileName);
+		//테스트용 경로
+		when(fileUploadService.getSavePath()).thenReturn(savePath);
+		when(multipartFile.getOriginalFilename()).thenReturn(originalFilename);
+		doNothing().when(multipartFile).transferTo(any(File.class));
 
-        assertNotNull(result);
-        assertEquals(file.getPath(), result.getPath());
-    }
+		String result = fileUploadService.uploadImage(new MultipartFile[]{multipartFile});
 
-    @Test
-    void testDownloadImageFileNotFound() {
-        String fileName = "nonexistent.jpg";
+		assertNotNull(result);
+		assertTrue(result.endsWith("_" + originalFilename)); // 결과 파일 이름이 기대대로 형성되었는지 확인
+		assertEquals(36 + 1 + originalFilename.length(), result.length());
+	}
 
-        RuntimeException exception = assertThrows(RuntimeException.class, () -> {
-            fileUploadService.downloadImage(fileName);
-        });
-
-        assertEquals("파일을 찾을 수 없습니다.", exception.getMessage());
-    }
-
-    @Test
-    void testGetFileContentSuccessfully() throws IOException {
-        // 테스트 파일 생성 및 내용 작성
-        File file = new File("test.txt");
-        Files.write(file.toPath(), "file content".getBytes());
-
-        // 테스트 수행
-        byte[] expectedContent = "file content".getBytes();
-        byte[] result = fileUploadService.getFileContent(file);
-
-        assertArrayEquals(expectedContent, result);
-
-        // 테스트 후 파일 삭제
-        file.delete();
-    }
-
-    @Test
-    void testGetFileContentFails() throws IOException {
-        // 존재하지 않는 파일 경로
-        File nonExistentFile = new File("non_existent_file.txt");
-
-        RuntimeException exception = assertThrows(RuntimeException.class, () -> {
-            fileUploadService.getFileContent(nonExistentFile);
-        });
+	@Test
+	void testUploadImageFails() throws IOException {
+		String savePath = System.getProperty("user.dir") + "/src/test/files/image";
 
 
-        assertEquals("파일을 읽는 도중 오류가 발생했습니다.", exception.getMessage());
-    }
+		when(multipartFile.getOriginalFilename()).thenReturn("test.jpg");
+		when(fileUploadService.getSavePath()).thenReturn(savePath);
+		doThrow(new IOException("File upload error")).when(multipartFile).transferTo(any(File.class));
+
+		RuntimeException exception = assertThrows(RuntimeException.class, () -> {
+			fileUploadService.uploadImage(new MultipartFile[]{multipartFile});
+		});
+
+		assertEquals("파일 업로드에 실패했습니다.", exception.getMessage());
+	}
+
+	@Test
+	void downloadImageSuccessfully() throws IOException {
+		String fileName = "test.jpg";
+		String savePath = System.getProperty("user.dir") + "/src/test/files/image";
+		boolean newFile = new File(savePath, fileName).createNewFile();// 실제 파일 생성
+
+		// Mock의 동작 정의
+		when(fileUploadService.getSavePath()).thenReturn(savePath);
 
 
-    @Test
-    void testGetSavePathWindows() {
-        String os = System.getProperty("os.name").toLowerCase();
-        System.setProperty("os.name", "Windows 10");
+		// downloadImage 메서드 호출
+		File result = fileUploadService.downloadImage(fileName);
 
-        String expectedPath = System.getProperty("user.dir") + "\\files\\image";
-
-        String result = fileUploadService.getSavePath();
-
-        assertEquals(expectedPath, result);
-
-        System.setProperty("os.name", os);
-    }
-
-    @Test
-    void testGetSavePathLinux() {
-        String os = System.getProperty("os.name").toLowerCase();
-        System.setProperty("os.name", "Linux");
-
-        String expectedPath = System.getProperty("user.dir") + "/files/image";
-
-        String result = fileUploadService.getSavePath();
-
-        assertEquals(expectedPath, result);
-
-        System.setProperty("os.name", os);
-    }
-
-    @Test
-    void testGetSavePathMac() {
-        String os = System.getProperty("os.name").toLowerCase();
-        System.setProperty("os.name", "Mac OS X");
-
-        String expectedPath = System.getProperty("user.dir") + "/files/image";
-
-        String result = fileUploadService.getSavePath();
-
-        assertEquals(expectedPath, result);
-
-        System.setProperty("os.name", os);
-    }
+		// 결과 검증
+		assertNotNull(result);
+		assertEquals(fileName, result.getName());
+		assertTrue(result.exists());
+	}
 
 
-    @Test
-    public void testUUIDGeneration() {
-        // When
-        String uuid = UUID.randomUUID().toString();
+	@Test
+	void testDownloadImageFileNotFound() {
+		String fileName = "nonexistent.jpg";
 
-        // Then
-        assertNotNull(uuid);  // UUID가 null이 아닌지 확인
-        assertEquals(36, uuid.length()); // UUID의 길이는 항상 36이어야 함
-        assertTrue(uuid.matches("^[a-f0-9\\-]{36}$"));  // UUID 형식이 맞는지 확인
-    }
+		RuntimeException exception = assertThrows(RuntimeException.class, () -> {
+			fileUploadService.downloadImage(fileName);
+		});
+
+		assertEquals("파일을 찾을 수 없습니다.", exception.getMessage());
+	}
+
+	@Test
+	void testGetFileContentSuccessfully() throws IOException {
+		// 테스트 파일 생성 및 내용 작성
+		File file = new File("test.txt");
+		Files.write(file.toPath(), "file content".getBytes());
+
+		// 테스트 수행
+		byte[] expectedContent = "file content".getBytes();
+		byte[] result = fileUploadService.getFileContent(file);
+
+		assertArrayEquals(expectedContent, result);
+
+		// 테스트 후 파일 삭제
+		file.delete();
+	}
+
+	@Test
+	void testGetFileContentFails() throws IOException {
+		// 존재하지 않는 파일 경로
+		File nonExistentFile = new File("non_existent_file.txt");
+
+		RuntimeException exception = assertThrows(RuntimeException.class, () -> {
+			fileUploadService.getFileContent(nonExistentFile);
+		});
+
+
+		assertEquals("파일을 읽는 도중 오류가 발생했습니다.", exception.getMessage());
+	}
+
+
+	@Test
+	void testGetSavePathWindows() {
+		String os = System.getProperty("os.name").toLowerCase();
+		System.setProperty("os.name", "Windows 10");
+
+		String expectedPath = System.getProperty("user.dir") + "\\files\\image";
+
+		String result = fileUploadService.getSavePath();
+
+		assertEquals(expectedPath, result);
+
+		System.setProperty("os.name", os);
+	}
+
+	@Test
+	void testGetSavePathLinux() {
+		String os = System.getProperty("os.name").toLowerCase();
+		System.setProperty("os.name", "Linux");
+
+		String expectedPath = System.getProperty("user.dir") + "\\files\\image";
+
+		String result = fileUploadService.getSavePath();
+
+		assertEquals(expectedPath, result);
+
+		System.setProperty("os.name", os);
+	}
+
+	@Test
+	void testGetSavePathMac() {
+		String os = System.getProperty("os.name").toLowerCase();
+		System.setProperty("os.name", "Mac OS X");
+
+		String expectedPath = System.getProperty("user.dir") + "\\files\\image";;
+
+		String result = fileUploadService.getSavePath();
+
+		assertEquals(expectedPath, result);
+
+		System.setProperty("os.name", os);
+	}
+
+
+	@Test
+	public void testUUIDGeneration() {
+		// When
+		String uuid = UUID.randomUUID().toString();
+
+		// Then
+		assertNotNull(uuid);  // UUID가 null이 아닌지 확인
+		assertEquals(36, uuid.length()); // UUID의 길이는 항상 36이어야 함
+		assertTrue(uuid.matches("^[a-f0-9\\-]{36}$"));  // UUID 형식이 맞는지 확인
+	}
 }
