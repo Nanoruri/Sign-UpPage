52d8105ecfe01f04e724a751e41c481311f1c63e
Nanoruri <kaby1217@gmail.com>
2024-10-24 16:34:12 +0900

    feat(JS): 게시글의 정보를 가져오는 editPost 추가
    - 게시글 상세보기 내 수정버튼을 클릭 시 해당 게시글의 정보를 가져오는 로직 구현
      -게시글의 정보를 가져와 게시글 수정에 쓰이기 위해 sessionStoage에 일시적으로 저장.
    
    - 게시판 메인페이지에 editPost.js파일 적용

---
+++ board/src/main/resources/static/js/editPost.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const editButton = document.getElementById('editButton');
+
+    editButton.addEventListener('click', function () {
+        fetchBoardDetails(currentPostId);
+    });
+});
+
+function fetchBoardDetails(boardId) {
+    const token = sessionStorage.getItem('aToken');
+
+    fetch(`/study/board/api/getBoardInfo/${boardId}`, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Failed to fetch board details');
+            }
+            return response.json();
+        })
+        .then(board => {
+            sessionStorage.setItem('boardToEdit', JSON.stringify(board));
+            window.location.href = '/study/board/page/postWrite';
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
