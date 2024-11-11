dccf8c61c0f6f913376ad0d254c7a01893662269
Nanoruri <kaby1217@gmail.com>
2024-11-11 17:44:45 +0900

    refactor(JS): 게시판 메인페이지 로그인 로직 개선
    - 기존 goLogin.js파일을 수정하여 단순 로그인 로직 변경
    - 로그인 여부에 따라 동적으로 로그인/로그아웃 버튼이 표시되도록 로직 개선.
    - 로그아웃 시 토큰 삭제 및 로그아웃 절차 진행

--- board/src/main/resources/static/js/goLogin.js
+++ board/src/main/resources/static/js/goLogin.js
@@ -1,9 +1,43 @@
-//boardIndex.html의 로그인 버튼을 누르면 goSignin.js의 코드가 실행된다.
-
 document.addEventListener('DOMContentLoaded', function () {
     const loginButton = document.getElementById('loginButton');
+    const isLoggedIn = sessionStorage.getItem('aToken') !== null;
 
-    loginButton.addEventListener('click', function () {
+    updateLoginButton(isLoggedIn);
+
+    function updateLoginButton(isLoggedIn) {
+        if (isLoggedIn) {
+            loginButton.textContent = '로그아웃';
+            loginButton.style.backgroundColor = '#ff4d4d'; // Set color for logout
+            loginButton.style.color = '#ffffff'
+            loginButton.removeEventListener('click', handleLogin);
+            loginButton.addEventListener('click', handleLogout);
+        } else {
+            loginButton.textContent = '로그인';
+            loginButton.style.backgroundColor = '#ffffff'; // Set color for login
+            loginButton.removeEventListener('click', handleLogout);
+            loginButton.addEventListener('click', handleLogin);
+        }
+    }
+
+    function handleLogin() {
         window.location.href = '/study/login';
-    });
+    }
+
+    function handleLogout() {
+        sessionStorage.removeItem('aToken');
+        fetch('/study/user/api/logout', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ refreshToken: sessionStorage.getItem('rToken') }),
+        }).then(response => {
+            if (response.ok) {
+                sessionStorage.removeItem('rToken');
+                window.location.href = '/study/board/page/boardIndex'
+            } else {
+                console.error('로그아웃 실패');
+            }
+        }).catch(error => {
+            console.error('서버 요청 중 오류 발생:', error);
+        });
+    }
 });
