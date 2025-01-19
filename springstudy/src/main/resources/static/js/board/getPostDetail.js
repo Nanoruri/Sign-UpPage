1125616b558dcf0a080705e48fa1b042d21469ba
Nanoruri <kaby1217@gmail.com>
2025-01-19 13:50:14 +0900

    refactor(JS): displayComment함수 로직 개선
    - 댓글 날짜를 기존 'date'에서 'updateDate' 기준으로 표시하도록 변경.
    - 댓글 내용 표시 시 수정 여부에 따라 내용을 동적으로 처리하도록 수정.

--- springstudy/src/main/resources/static/js/board/getPostDetail.js
+++ springstudy/src/main/resources/static/js/board/getPostDetail.js
@@ -81,7 +81,7 @@ function displayComments(comments, token, currentUserId) {
             headerDiv.classList.add('comment-header');
 
             const date = document.createElement('span');
-            date.textContent = new Date(comment.date).toLocaleString();
+            date.textContent = new Date(comment.updateDate).toLocaleString();
             date.classList.add('comment-date');
 
             // 댓글 작성자 표시
@@ -95,7 +95,7 @@ function displayComments(comments, token, currentUserId) {
 
             const contentDiv = document.createElement('div');
             contentDiv.classList.add('comment-content');
-            contentDiv.textContent = comment.content;
+            contentDiv.textContent = isUpdateComment(comment);
 
             // 수정 버튼 생성
             const commentEditButton = document.createElement('button');
