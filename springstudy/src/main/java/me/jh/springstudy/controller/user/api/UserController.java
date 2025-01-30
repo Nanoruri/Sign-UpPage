e3091ad817596bda8404ba5262e479c3e607bd7c
Nanoruri <kaby1217@gmail.com>
2025-01-30 20:36:12 +0900

    refactor: ValidationController 제거
    - ValidationController 제거
    - 기존 ValidationController 내 API -> UserController로 이전
    - checkDuplicateUserId의 메서드 엔드포인트 findId의 엔드포인트와 중복되어 '/userId'엔드포인트로 변경

--- springstudy/src/main/java/me/jh/springstudy/controller/user/api/UserController.java
+++ springstudy/src/main/java/me/jh/springstudy/controller/user/api/UserController.java
@@ -181,4 +181,57 @@ public class UserController {
         response.put("message", "토큰 인증 실패.");
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
     }
+
+    /**
+     * 사용자 ID가 중복인지 확인하는 API.
+     * 사용자 ID가 중복되면 CONFLICT 상태와 메시지를 반환하고, 중복이 아니면 OK 상태와 메시지를 반환.
+     *
+     * @param reqData 중복 여부를 확인할 사용자 ID
+     * @return 중복 여부에 따른 ResponseEntity. 중복되면 CONFLICT 상태와 메시지를 반환하고, 중복이 아니면 OK 상태와 메시지를 반환합니다.
+     * @throws UserException 사용자 ID가 중복될 경우 아이디 중복 오류 메세지를 반환
+     * @implNote 이 메서드는 {@link UserService#isDuplicateId(String)}를 사용하여 사용자 ID 중복 여부를 확인.
+     */
+    @Operation(summary = "사용자 ID 중복 체크", description = "사용자 ID가 중복되는지 확인합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "사용 가능한 ID입니다."),
+            @ApiResponse(responseCode = "409", description = "중복된 ID입니다.")
+    })
+    @PostMapping("/userId")
+    public ResponseEntity<String> checkDuplicateUserId(@RequestBody Map<String, String> reqData) {
+        String userId = reqData.get("userId");
+
+        if (userService.isDuplicateId(userId)) {
+            log.warn("중복된 ID 발견.DB 확인 요망");
+            throw new UserException(UserErrorType.ID_ALREADY_EXIST);
+        }
+        log.info("ID 중복검사 성공");
+        return ResponseEntity.ok("사용가능한 ID입니다.");//중복X(false) = http ok(200)상태와 함께 메세지 출력
+    }
+
+    /**
+     * 사용자 Email이 중복인지 확인하는 API.
+     * 사용자 Email이 중복되면 CONFLICT 상태와 메시지를 반환하고, 중복이 아니면 OK 상태와 메시지를 반환.
+     *
+     * @param reqData 중복 여부를 확인할 사용자 Email
+     * @return 중복 여부에 따른 ResponseEntity. 중복되면 CONFLICT 상태와 메시지를 반환하고, 중복이 아니면 OK 상태와 메시지를 반환합니다.
+     * @throws UserException 사용자 Email이 중복될 경우 이메일 중복 오류 메세지를 반환
+     * @implNote 이 메서드는 {@link UserService#isDuplicateEmail(String)}를 사용하여 사용자 Email 중복 여부를 확인.
+     */
+    @Operation(summary = "사용자 Email 중복 체크", description = "사용자 Email이 중복되는지 확인합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "사용 가능한 이메일입니다."),
+            @ApiResponse(responseCode = "409", description = "중복된 이메일입니다.")
+    })
+    @PostMapping("/email")
+    public ResponseEntity<String> checkDuplicateEmail(@RequestBody Map<String, String> reqData) {
+        String email = reqData.get("email");
+
+        if (userService.isDuplicateEmail(email)) {
+            log.warn("중복된 Email 발견.DB 확인 요망");
+            throw new UserException(UserErrorType.USER_ALREADY_EXIST);
+        }
+        log.info("Email 중복검사 성공");
+        return ResponseEntity.ok("사용가능한 이메일입니다.");
+    }
+
 }
